3b7fbf55d97ab7c3422b196add00e82ac777ad38
Pankaj Sharma <pankajsharma0508@hotmail.com>
2024-09-22 15:25:20 +0200

    Added basic step.

--- tasks/load_dataset.py
+++ tasks/load_dataset.py
@@ -1,11 +1,15 @@
-import pandas as pd;
+import pandas as pd
 import logging
 
 # Configure standard logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
 # Load the dataset
 df = pd.read_csv("../data/matches.csv")
 logger.info("Dataset loaded successfully.")
 logger.info(f"DataFrame head:\n{df.head()}")
+
+print(df)
